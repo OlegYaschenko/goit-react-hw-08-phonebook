5dce7f7f3d3c882c6c99602c15e39302a613bf0d
OlegYaschenko <olegyaschenko@gmail.com>
2022-04-29 04:16:26 +0300

    add LocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,35 @@ import Section from 'components/Section/Section';
 
 class App extends Component {
   state = {
-    contacts: [
+    defaultContacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    } else {
+      this.setState({ contacts: this.state.defaultContacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const prevContacts = prevState.contacts;
+    const nextContacts = this.state.contacts;
+
+    if (prevContacts !== nextContacts) {
+      localStorage.setItem('contacts', JSON.stringify(nextContacts));
+    }
+  }
 
   addContact = ({ name, number }) => {
     const { contacts } = this.state;
